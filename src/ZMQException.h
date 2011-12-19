0695a0002946fdf6abc71bb54a3b7556f13bc299
Massimo Gengarelli <gengarel@cs.unibo.it>
2011-12-19 02:21:21 +0100

    New initializer for ZMQException

--- src/ZMQException.h
+++ src/ZMQException.h
@@ -12,6 +12,7 @@
     @private int _error_code;
 }
 
+-(id)initWithCode: (NSString *)aReason code:(int)errorCode;
 -(void) setErrorCode:(int) error_code;
 -(int) getErrorCode;
 
--- src/ZMQException.m
+++ src/ZMQException.m
@@ -20,4 +20,14 @@
     _error_code = error_code;
 }
 
+- (id)initWithCode:(NSString *)aReason code:(int)errorCode
+{
+    self = [[ZMQException alloc] initWithName:@"ZMQException" 
+                                       reason:aReason 
+                                     userInfo:nil];
+    _error_code = errorCode;
+    
+    return self;
+}
+
 @end
